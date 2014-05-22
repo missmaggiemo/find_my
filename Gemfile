3633a8ae8edbcd04c0468011858cd98905d0b91f
missmaggiemo <margaret.m.moreno@gmail.com>
2014-05-22 14:38:29 -0700

    messing with production env

--- Gemfile
+++ Gemfile
@@ -71,4 +71,5 @@ end
 
 group :production do
   gem 'rails_12factor', '0.0.2'
+  gem 'rails_serve_static_assets'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ YelpApp::Application.configure do
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
+  # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
@@ -20,16 +20,14 @@ YelpApp::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # Nope. Fallback.
-  config.assets.compile = true
-  
-    # Compress JavaScripts and CSS.
+  # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -68,7 +66,7 @@ YelpApp::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
+  # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
@@ -79,9 +77,13 @@ YelpApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-
+  
+  
+  
+  
+  # SET HEROKU PRODUCTION VARS BEFORE PUSHING TO PRODUCTION
+  
+  
+  
   
 end
