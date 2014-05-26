636eb48fe40c6c6f072d0f18263143a662c7101f
missmaggiemo <margaret.m.moreno@gmail.com>
2014-05-26 11:59:36 -0700

    username search doesn't need login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:feed]
   
   def show
     @user = User.find(params[:id])
